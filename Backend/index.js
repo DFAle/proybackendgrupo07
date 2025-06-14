e131a6b1691467e309d495c028c15cb073a56450
x6Nahuel <gaspargonzalo024@gmail.com>
2025-06-14 19:45:22 -0300

    gonza

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ app.use(cors({origin: 'http://localhost:4200'}));
 
 //Cargamos el modulo de direccionamiento de rutas
 //app.use('/api/agente', require('./routes/agente.route.js'));
-//app.use('/api/sector', require('./routes/sector.route'));
+app.use('/api/socio', require('./routes/socio.route'));
 app.set('port', process.env.PORT || 3000);
 //starting the server
 app.listen(app.get('port'), () => {
