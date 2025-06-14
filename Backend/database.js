f92438dc47e315b8cea784586c36361e523cff16
DFAle <aale0800z@gmail.com>
2025-06-14 19:12:20 -0300

    ale

--- Backend/database.js
+++ Backend/database.js
@@ -1,4 +1,6 @@
+require('dotenv').config(); // Carga las variables del .env
 const mongoose = require('mongoose');
+const URI = process.env.url;
 //const URI = 'mongodb://localhost/proyectoFinal';
 mongoose.connect(URI)
 .then(db=>console.log('DB is connected'))
