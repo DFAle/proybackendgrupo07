60754a92739f3a93086d58f77c5720a34c91eb4f
DFAle <aale0800z@gmail.com>
2025-06-14 19:04:00 -0300

    ale

--- Backend/database.js
+++ Backend/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb://localhost/proyectoFinal';
+//const URI = 'mongodb://localhost/proyectoFinal';
 mongoose.connect(URI)
 .then(db=>console.log('DB is connected'))
 .catch(err=>console.error(err))
--- Backend/index.js
+++ Backend/index.js
@@ -9,8 +9,6 @@ app.use(cors({origin: 'http://localhost:4200'}));
 //Cargamos el modulo de direccionamiento de rutas
 //app.use('/api/agente', require('./routes/agente.route.js'));
 //app.use('/api/sector', require('./routes/sector.route'));
-//setting
-
 app.set('port', process.env.PORT || 3000);
 //starting the server
 app.listen(app.get('port'), () => {
