e955f284bdb120afce185c1eb282d723ca1b87f5
Federico Agustin Salinas <fedeasalinas@gmail.com>
2025-06-20 18:12:03 -0300

    Se modifica el modelo de usuario

--- Backend/models/usuario.js
+++ Backend/models/usuario.js
@@ -5,12 +5,11 @@ const UsuarioSchema = new Schema({
   nombre: { type: String, required: true },
   apellido: { type: String, required: true },
   dni: { type: String, required: true },
-  correo: { type: String, required: true },
-  username: { type: String, required: true,unique: true,trim: true,lowercase: true, },
+  username: { type: String, required: true, unique: true, trim: true, lowercase: true, },
   correo: { type: String, required: true },
   password: { type: String, required: true },
-  activo:   { type: Boolean, required: true },
-  rol:      {type: Schema.Types.ObjectId,ref: rol,required: true}, 
+  activo: { type: Boolean, required: true },
+  rol: { type: Schema.Types.ObjectId, ref: rol, required: true },
 });
 
 module.exports = mongoose.models.Usuario || mongoose.model("Usuario", UsuarioSchema);
