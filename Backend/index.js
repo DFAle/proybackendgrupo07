f5e29a9e699aff176a2d250b5d904ae1c2559cfd
Federico Agustin Salinas <fedeasalinas@gmail.com>
2025-06-20 17:49:53 -0300

    Se agrega la ruta en el archivo index.js  para los endpoints de rol

--- Backend/index.js
+++ Backend/index.js
@@ -11,6 +11,7 @@ app.use(cors({origin: 'http://localhost:4200'}));
 app.use('/api/socio', require('./routes/socio.route'));
 app.use('/api/usuario', require('./routes/usuario.route.js'));
 app.use('/api/actividad', require('./routes/actividad.route.js'));
+app.use('/api/rol', require('./routes/rol.route.js'));
 app.set('port', process.env.PORT || 3000);
 //starting the server
 app.listen(app.get('port'), () => {
