97b14aaa365ddcf08159bc351c4448ba1ee2552e
Federico Agustin Salinas <fedeasalinas@gmail.com>
2025-06-20 17:46:27 -0300

    Se modifica el modelo de rol

--- Backend/models/rol.js
+++ Backend/models/rol.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-const UsuarioSchema = new Schema({
-  id: { type: String, required: true},
+const RolSchema = new Schema({
+  id: { type: String, required: true },
   tipo: { type: String, required: true },
-  //Administrador, Profesor o Socio
 });
 
-module.exports = mongoose.models.Usuario || mongoose.model("Usuario", UsuarioSchema);
+module.exports = mongoose.models.RolSchema || mongoose.model("Rol", RolSchema);
