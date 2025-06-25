0274fb86f1dcac4af385247b9b3e27af04a46ecd
x6Nahuel <gaspargonzalo024gmail.com>
2025-06-25 15:39:05 -0300

    addfindProfesor

--- Backend/routes/profesor.route.js
+++ Backend/routes/profesor.route.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/',profesorCtrl.getProfesor);
+router.get('/:id',profesorCtrl.getProfesorById);
 router.post('/',profesorCtrl.createProfesor);
 router.delete('/:id',profesorCtrl.deleteProfesor);
 router.put('/:id',profesorCtrl.editProfesor);
