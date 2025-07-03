400d0527d149a89d56d2b78f539a5a8a4b5f9320
Nahuel Alberto Cachambi <cachasan.10@gmail.com>
2025-07-03 01:05:02 -0300

    Se creo la documentacion con swagger
    Se realizo la documentacion para los modelos:
    profesor,actividad,rol,usuario.
    Se documento el controlador de usuario.

--- Backend/index.js
+++ Backend/index.js
@@ -32,6 +32,11 @@ app.use('/api/profesor', require('./routes/profesor.route.js'));
 //rutas para mercado pago
 app.use('/api/mp', require('./routes/mp.route.js'));
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger_output.json');
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+
 app.set('port', process.env.PORT || 3000);
 //starting the server
 app.listen(app.get('port'), () => {
