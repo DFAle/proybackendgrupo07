cf9b4807cfc58cb59457d3270a00204885ce91dd
Federico Agustin Salinas <fedeasalinas@gmail.com>
2025-07-03 21:31:59 -0300

    Subiendo modificación del controlador de actividades

--- Backend/controllers/actividad.controller.js
+++ Backend/controllers/actividad.controller.js
@@ -89,7 +89,7 @@ actividadCtrl.inscribirUsuario = async (req, res) => {
 
         actividad.inscriptos.push(usuarioId);
         await actividad.save();
-        
+
         await RegistroActividad.create({
             usuario: usuarioId,
             actividad: id,
@@ -138,12 +138,10 @@ actividadCtrl.darDeBajaUsuario = async (req, res) => {
 actividadCtrl.actividadesDeUsuario = async (req, res) => {
     const { userId } = req.params;
     try {
-        const actividades = await Actividad.find().populate('inscriptos');
-
+        const actividades = await Actividad.find({ inscriptos: userId });
         if (actividades.length === 0) {
             return res.status(404).json({ msg: 'El usuario no está inscripto en ninguna actividad' });
         }
-
         res.status(200).json(actividades);
     } catch (error) {
         res.status(500).json({
